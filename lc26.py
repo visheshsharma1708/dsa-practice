907570bb7d793e668aa693fc114f5d6534e0dbfd
vishesh1708 <170830119+visheshsharma1708@users.noreply.github.com>
2026-09-02 20:27:30 +0530

    practice

---
+++ lc26.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeDuplicates(self, nums):
+        if not nums:
+            return 0
+
+        i = 1
+
+        for j in range(1, len(nums)):
+            if nums[j] != nums[j - 1]:
+                nums[i] = nums[j]
+                i += 1
+
+        return i
