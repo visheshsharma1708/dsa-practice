f550d2d5500e2d8bd28db513d512c66c6ddbd739
vishesh1708 <170830119+visheshsharma1708@users.noreply.github.com>
2026-09-05 22:20:23 +0530

    practice

---
+++ lc31.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        n = len(nums)
+
+        i = n - 2
+
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+
+        if i >= 0:
+            j = n - 1
+
+            while nums[j] <= nums[i]:
+                j -= 1
+
+            nums[i], nums[j] = nums[j], nums[i]
+
+        left = i + 1
+        right = n - 1
+
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
