7389ecf6a33059d967cae506b8ae4f1237e7f4d3
vishesh1708 <170830119+visheshsharma1708@users.noreply.github.com>
2026-09-07 21:20:53 +0530

    practice

---
+++ lc34.py
@@ -0,0 +1,39 @@
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        def find_first():
+            left = 0
+            right = len(nums) - 1
+            ans = -1
+
+            while left <= right:
+                mid = (left + right) // 2
+
+                if nums[mid] == target:
+                    ans = mid
+                    right = mid - 1
+                elif nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+
+            return ans
+
+        def find_last():
+            left = 0
+            right = len(nums) - 1
+            ans = -1
+
+            while left <= right:
+                mid = (left + right) // 2
+
+                if nums[mid] == target:
+                    ans = mid
+                    left = mid + 1
+                elif nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+
+            return ans
+
+        return [find_first(), find_last()]
