8c2f0aa64bd97eb3d27ce0ad7039879346571199
vishesh1708 <170830119+visheshsharma1708@users.noreply.github.com>
2026-09-13 22:24:53 +0530

    practice

--- lc28.py
+++ lc28.py
@@ -1,10 +1,13 @@
+# class Solution:
+#     def strStr(self, haystack: str, needle: str) -> int:
+#         n = len(haystack)
+#         m = len(needle)
+
+#         for i in range(n - m + 1):
+#             if haystack[i:i + m] == needle:
+#                 return i
+
+#         return -1
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        n = len(haystack)
-        m = len(needle)
-
-        for i in range(n - m + 1):
-            if haystack[i:i + m] == needle:
-                return i
-
-        return -1
+        return haystack.find(needle)
