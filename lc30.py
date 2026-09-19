1816c4eb07c756121a0504397bd4fec766157590
vishesh1708 <170830119+visheshsharma1708@users.noreply.github.com>
2026-09-19 19:42:45 +0530

    practice

---
+++ lc30.py
@@ -0,0 +1,45 @@
+class Solution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        word_len = len(words[0])
+        word_count = len(words)
+
+        target = {}
+
+        for word in words:
+            target[word] = target.get(word, 0) + 1
+
+        ans = []
+
+        for offset in range(word_len):
+            left = offset
+            right = offset
+            current = {}
+            count = 0
+
+            while right + word_len <= len(s):
+                word = s[right:right + word_len]
+                right += word_len
+
+                if word not in target:
+                    current.clear()
+                    count = 0
+                    left = right
+                    continue
+
+                current[word] = current.get(word, 0) + 1
+                count += 1
+
+                while current[word] > target[word]:
+                    left_word = s[left:left + word_len]
+                    current[left_word] -= 1
+                    left += word_len
+                    count -= 1
+
+                if count == word_count:
+                    ans.append(left)
+                    left_word = s[left:left + word_len]
+                    current[left_word] -= 1
+                    left += word_len
+                    count -= 1
+
+        return ans
