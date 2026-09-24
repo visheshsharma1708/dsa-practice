ce7a67fc5601c61e7cd54a43eff820e80732f2f4
vishesh1708 <170830119+visheshsharma1708@users.noreply.github.com>
2026-09-24 20:21:25 +0530

    practice

---
+++ lc118.py
@@ -0,0 +1,13 @@
+class Solution:
+    def generate(self, numRows: int) -> list[list[int]]:
+        ans = []
+
+        for i in range(numRows):
+            row = [1] * (i + 1)
+
+            for j in range(1, i):
+                row[j] = ans[i - 1][j - 1] + ans[i - 1][j]
+
+            ans.append(row)
+
+        return ans
